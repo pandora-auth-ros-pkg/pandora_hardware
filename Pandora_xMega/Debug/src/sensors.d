50e6b2fc76b84b50deda71747665d41ac4d78895
Konstantinos Panayiotou <glagloui@gmail.com>
2014-03-16 19:22:51 +0200

    ""

--- Pandora_xMega/Debug/src/sensors.d
+++
@@ -1,9 +0,0 @@
-src/sensors.d src/sensors.o: ../src/sensors.c ../src/Include/sensors.h \
- c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1051\avr8-gnu-toolchain\lib\gcc\avr\4.8.1\include\stdint.h \
- c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1051\avr8-gnu-toolchain\avr\include\stdint.h
-
-../src/Include/sensors.h:
-
-c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1051\avr8-gnu-toolchain\lib\gcc\avr\4.8.1\include\stdint.h:
-
-c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1051\avr8-gnu-toolchain\avr\include\stdint.h:
