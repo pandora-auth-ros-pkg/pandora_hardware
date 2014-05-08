3be728c925985e6f030b15802f75ebd70e1faf4d
orestis <orestis@ee.auth.gr>
2014-05-08 18:12:34 +0300

    Created doxy mainpage, moved general comments to apropriate locations, doxified fault handlers

--- headM4/LibMods/i2c_nonblocking.h
+++ headM4/LibMods/i2c_nonblocking.h
@@ -1,23 +1,25 @@
 /** @file
  * @author Orestis Zachariadis
- * @brief
+ *
+ * @brief Implements I2C communication with interrupts
+ *
+ * @b Instructions:
+ * @li Include this file in i2c_api.c and disable i2c_clear_SI() and i2c_wait_SI() in i2c_api.c.
+ * @li Call I2C?_queue_create()
+ *
+ * @note
+ * Important note: due to MBED Library bug, i2c_write() and i2c_read() in i2c_api.c were changed in order
+ * to support a repeated start i2c_read after an i2c_write call.
+ * @n @n
+ * Instructions if you want to use repeated start:
+ * @li Delete original i2c_api.c and use i2c_api2.c in LibMods folder instead.
+ * @li If you set "repeated" argument to true in I2C::write() the following I2C::read() must also
+ * have "repeated" argument set to true.
+ * @li An I2C transaction is not allowed to start with I2C::read() with "repeated" argument set to true.
+ *
+ * @attention As of now only the sequence write -> repeated start -> read is implemented.
+ * @note Changes in original i2c_api.c are marked with "CHANGED MBED LIBRARY HERE" in comments.
  */
-
-//Instructions: 1) Include this file in i2c_api.c and disable i2c_clear_SI() and i2c_wait_SI() in i2c_api.c.
-//2) In main()  call I2C?_queue_create()
-//Important note: due to MBED Library bug i2c_write() and i2c_read() in i2c_api.c were changed, in order
-//-> to support a repeated start i2c_read after an i2c_write call. If you want repeated start functionality
-//-> delete original i2c_api.c and use i2c_api2.c in LibMods folder instead. Also there will be other additions in
-//-> i2c_api2.c. All changes are marked with "CHANGED MBED LIBRARY HERE" in comments.
-//Instructions if you want to use repeated start: 1) If you set "repeated" argument to true in I2C::write() the
-//->  following I2C::read() must also have "repeated" argument set to true. 2) An I2C transaction is not allowed to
-//-> start with I2C::read() with "repeated" argument set to true.
-//Note: As of now only the sequence write -> repeated start -> read is implemented.
-
-//Things to note: 1) Interrupts must be enabled in NVIC too (I2C is enabled only in NVIC), 2) NVIC_SetPriority() is
-//-> optional, default priority is 0 (highest), 3) if we want to choose the ISR during runtime, dynamic vectors must
-//-> be used (MBED has NVIC_SetVector() implemented for this task), 4) interrupt priority registers for interrupts 32-40 don't appear to LPCExpresso's
-//-> Peripheral+ view, but they exist if we check the memory manually
 
 #ifndef I2C_NONBLOCKING_H_
 #define I2C_NONBLOCKING_H_
@@ -28,16 +30,14 @@
 #include "error.h"
 #include "objects.h"
 
-#define SI 3	///I2C Serial Interrupt flag
+#define SI 3	///<I2C Serial Interrupt flag
 
 typedef struct i2c_s i2c_t;
 
 
 //If queue.put() is executed inside an ISR, and we have continuous interrupts (not allowing non-ISR code to run),
 //->queue uses a size 16 FIFO (independent from what the user sets). Shouldn't happen in a real case scenario.
-//Things to note: 1) extern "C" is required if handler executed in C++ code, 2) Interrupt flag should be cleared
-//-> or we will enter the ISR continuously, 3) an ISR can't be interrupted by the same interrupt that caused it,
-//->only by a higher priority one.
+
 void I2C0_IRQHandler();
 
 void I2C1_IRQHandler();
@@ -46,10 +46,10 @@ void I2C1_IRQHandler();
 extern "C" {
 #endif
 
-/// Create and Initialize I2C0 interrupt Message Queue
+/** @brief Create and Initialize I2C0 interrupt Message Queue */
 void I2C0_queue_create();
 
-/// Create and Initialize I2C1 interrupt Message Queue
+/** @brief Create and Initialize I2C1 interrupt Message Queue */
 void I2C1_queue_create();
 
 #ifdef __cplusplus
